130b3594176985aacc7806cdba49707d7697c2f5
AlexandraIlie <alexandra.ilie77@gmail.com>
2022-03-20 19:37:53 +0200

    Initial commit

---
+++ src/test/java/com/endava/school/petstoreapi/PetStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.school.petstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
